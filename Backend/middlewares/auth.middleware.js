def3e22f16138d19698dcaac32d50af3a81e3f08
rajpatidar12 <patidarraj465@gmail.com>
2025-04-18 19:04:38 +0530

    minor changes

---
+++ Backend/middlewares/auth.middleware.js
@@ -0,0 +1,27 @@
+const userModel = require("../models/user.modal");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+module.exports.authUser = async (req, res, next) => {
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  const isBlacklisted = await userModel.findOne({ token: token });
+  if (isBlacklisted) {
+    return res.status(401).json({ message: "unauthorized" });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await userModel.findById(decoded._id);
+
+    if (!user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
